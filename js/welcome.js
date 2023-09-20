a09b342863ff5e2565fd1a5bfce6248b6c04ac09
ZeroHenriot <darii.catalin@gmail.com>
2023-09-20 09:28:06 +0200

    Welcome page js

--- js/welcome.js
+++ js/welcome.js
@@ -1,1 +1,25 @@
+const form = document.getElementById('myForm')
+const checkbox = document.querySelector('#myCheckbox')
+const button = document.querySelector('#proceedButton')
 
+checkbox.checked = false
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault() // Evita l'invio predefinito del modulo
+
+  // Controlla se la checkbox è selezionata
+  if (checkbox.checked) {
+    // Reindirizza a benchmark.html se la checkbox è selezionata
+    window.location.href = 'benchmark.html'
+  }
+})
+
+checkbox.addEventListener('click', function () {
+  // Abilita il pulsante se la checkbox è selezionata
+  if (checkbox.checked) {
+    proceedButton.removeAttribute('disabled')
+  } else {
+    // Disabilita il pulsante se la checkbox non è selezionata
+    proceedButton.setAttribute('disabled', 'true')
+  }
+})
